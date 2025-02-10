d03f7f0b498d4269679194fdb749a3991a42bf24
Sanapol <junrey@list.ru>
2025-02-10 23:19:20 +0300

    update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,13 @@
 plugins {
     id("java")
+    id("com.github.ben-manes.versions") version "0.48.0"
+    application
+    id("io.freefair.lombok") version "8.6"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+application {
+    mainClass.set("hexlet.code.App")
 }
 
 group = "hexlet.code"
@@ -10,6 +18,16 @@ repositories {
 }
 
 dependencies {
+    implementation("com.h2database:h2:2.2.220")
+    implementation("com.zaxxer:HikariCP:5.0.1")
+    implementation("io.javalin:javalin:6.1.3")
+    implementation("gg.jte:jte:3.1.9")
+    implementation("io.javalin:javalin-rendering:6.1.3")
+    implementation("io.javalin:javalin-bundle:6.1.3")
+    implementation("org.slf4j:slf4j-simple:2.0.7")
+    implementation("net.datafaker:datafaker:2.0.1")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.0")
+    testImplementation("org.assertj:assertj-core:3.23.1")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
