5aff9288d6218cbca0d3543ebc52b32ff5ef9a8f
Sanapol <junrey@list.ru>
2025-02-15 08:13:04 +0300

    add BasePage.java

---
+++ app/src/main/java/hexlet/code/websites/BasePage.java
@@ -0,0 +1,15 @@
+package hexlet.code.websites;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class BasePage {
+    private String flash;
+    private String flashType;
+}
