34daed5c97632778acc2ff56ed1deb7e90623cde
Sanapol <junrey@list.ru>
2025-03-06 18:17:07 +0300

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -26,7 +26,7 @@ public class App {
     public static void main(String[] args) throws SQLException {
         Javalin app = getApp();
         String port = System.getenv().getOrDefault("PORT", "7070");
-        app.start(Integer.parseInt(port));
+        app.start(Integer.valueOf(port));
     }
 
     private static TemplateEngine createTemplateEngine() {
